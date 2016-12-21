30ad57e21c0c366019d02359cc5cfe9550cfdd5c
Riccardo Miccini <riccardo.miccini@gmail.com>
2016-12-21 12:52:27 +0100

    fixed 3d plot

--- quad_draw_example.py
+++ quad_draw_example.py
@@ -1,21 +1,21 @@
 import sys
 import time
 import numpy as np
+from visualizeQuad import QuadPlotter
 from matplotlib import pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 def load_src(name, fpath):
     import os
     import imp
-    print(os.path.join(os.path.dirname(__file__), fpath))
     return imp.load_source(name, os.path.join(os.path.dirname(__file__), fpath))
 
 mt = load_src('mathTools', 'quaternion/quaternion.py')
 
 
-if len(sys.argv) == 2:
+if len(sys.argv) == 3:
   if sys.argv[1] == 'serial':
     from serial_reader import Reader
-    reader = Reader(port='/dev/ttyUSB0', baudrate=115200)
+    reader = Reader(port='/dev/ttyUSB0', baudrate=57600)
 
   elif sys.argv[1] == 'socket':
     from socket_reader import Reader
@@ -31,14 +31,8 @@ else:
   sys.exit()
 
 
-plotter = QuadPlotter()
+plotter = QuadPlotter(reader=reader, quaLabel=label)
 
 lastPlotUpdate = 0
 while True:
-  s, data, succes = reader()
-  if succes and s == label:
-    q = mt.Quaternion(*data[:4])
-    pos = data[4:7]
-    if time.time() - lastPlotUpdate > 0.03:
-      plotter.drawQuad(q, pos)
-      lastPlotUpdate = time.time()
+  plotter.update()
--- visualizeQuad.py
+++ visualizeQuad.py
@@ -4,13 +4,21 @@ import numpy as np
 from matplotlib import pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
+def load_src(name, fpath):
+    import os
+    import imp
+    return imp.load_source(name, os.path.join(os.path.dirname(__file__), fpath))
+
+mt = load_src('mathTools', 'quaternion/quaternion.py')
+
 class QuadPlotter(object):
   def __init__(self, reader, quaLabel, posLabel=None):
+    self.reader = reader
     self.quaLabel = quaLabel
     self.posLabel = posLabel
     self.fig = plt.figure()
     self.fig.canvas.mpl_connect('key_press_event', self.press)
-    self.ax = fig.add_subplot(1, 1, 1, aspect='equal', projection='3d')
+    self.ax = self.fig.add_subplot(1, 1, 1, aspect='equal', projection='3d')
     self.lines = [self.ax.plot([], [], [], 'r', linewidth=3)[0],
                   self.ax.plot([], [], [], 'g', linewidth=3)[0],
                   self.ax.plot([], [], [], 'b', linewidth=3)[0]]
@@ -48,12 +56,15 @@ class QuadPlotter(object):
     self.lines[2].set_ydata(lineZ[1, :] + pos[1])
     self.lines[2].set_3d_properties(lineZ[2, :] + pos[2])
 
+    plt.pause(0.001)
+
   def update(self):
     s, data, isNumerical = self.reader()
-    if succes and s == quaLabel:
-      self.latest = mt.Quaternion(*data)
+    #print(s, data, self.quaLabel, isNumerical)
+    if isNumerical and s == self.quaLabel:
+      self.latestQua = mt.Quaternion(*data)
       if time.time() - self.timeAtLastPlotUpdate > self.timeBetweenPlotUpdates:
         self.drawQuad(self.latestQua, self.latestPos)
         self.timeAtLastPlotUpdate = time.time()
-    elif succes and self.posLabel is not None and label == self.posLabel:
+    elif isNumerical and self.posLabel is not None and label == self.posLabel:
       self.latestPos = data
