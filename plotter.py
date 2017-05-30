93e9253d3e506876e834ac1412182b5fdf7b88b6
erikbrntsn <erikbrntsn@gmail.com>
2017-05-30 12:58:37 +0200

    serial_port instead port (bug fix)

--- plotter.py
+++ plotter.py
@@ -33,7 +33,7 @@ def startSocketPlotter(args):
 
 def startSerialPlotter(args):
   import serial_reader
-  reader = serial_reader.Reader(port=args.port, baudrate=args.baudrate)
+  reader = serial_reader.Reader(port=args.serial_port, baudrate=args.baudrate)
   return Plotter(reader=reader, ringLength=args.n_points, labels=args.labels)
 
 
